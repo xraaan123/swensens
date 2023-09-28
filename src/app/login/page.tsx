fc3bfba6416daf050e75c1b297b17f913b148f6c
Waroon <waroon1999@hotmail.com>
2023-09-29 01:25:53 +0700

    First update

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "antd";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <main>
+      <h1>Login Page</h1>
+    </main>
+  );
+}
